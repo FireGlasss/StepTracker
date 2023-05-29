e4e9ac8193bda0f7cf00fcea557e58a0cf6832f9
FireGlasss <trofim419@gmail.com>
2023-05-29 13:40:57 +0300

    Delete Report

--- src/Tuitering/Converter.java
+++ src/Tuitering/Converter.java
@@ -3,35 +3,16 @@ package Tuitering;
 public class Converter {
 
 
-    private static int smStep = 75;
-    private static int calories = 50;
+    private final static int smStep = 75;
+    private final static int calories = 50;
 
 
     public static double stepToKM(double summaStepsForMonth) {
-        double km = (summaStepsForMonth * 75)/100000;
-        return km;
+        return (summaStepsForMonth * smStep)/100000;
+
     }
 
     public static int stepToCalories(int summaStepsForMonth) {
-        int Kcalories = (summaStepsForMonth * 50) / 1000;
-        return Kcalories;
-
+        return ( summaStepsForMonth * calories) / 1000;
     }
-
-    public static int getSmStep() {
-        return smStep;
-    }
-
-    public static void setSmStep(int smStep) {
-        Converter.smStep = smStep;
-    }
-
-    public static int getCalories() {
-        return calories;
-    }
-
-    public static void setCalories(int calories) {
-        Converter.calories = calories;
-    }
-
 }
